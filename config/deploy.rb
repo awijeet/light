15a971763c67a52d10117b61c200ba7f54bf554e
Awijeet Shandilya <avijit.shandilya@gmail.com>
2012-05-12 03:45:58 +0530

    data work

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,26 @@
-require "rvm/capistrano"
-require 'bundler/capistrano'
-load 'deploy/assets'
-
-set :rvm_type, :user  # Don't use system-wide RVM
-set :bundle_flags, ""
-set :bundle_dir, ""
-
-set :user, "root"
-set :use_sudo, false
-
 set :application, "rubyonrails4ror"
 set :repository,  "git@github.com:awijeet/light.git"
 set :scm, :git
-set :scm_command, "/opt/local/bin/git" 
-set :local_scm_command, "git" 
 set :branch, 'master'
 set :deploy_to, "/var/apps/#{application}"
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, "208.111.45.210"                          # Your HTTP server, Apache/etc
 role :app, "208.111.45.210"                          # This may be the same as your `Web` server
-role :db,  "208.111.45.210", primary: true # This is where Rails migrations will run
-before 'deploy:finalize_update', 'x:set_current_release'
-task :set_current_release, :roles => :app do
-    set :current_release, latest_release
-end
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, roles: :app, except: { no_release: true } do
-    run "touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
+role :db,  "208.111.45.210", :primary => true # This is where Rails migrations will run
+role :db,  "208.111.45.210"
+
+# if you want to clean up old releases on each deploy uncomment this:
+ after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+ namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
