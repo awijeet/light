936fb82a8f3841069074e8eb3866be24dce02e43
Awijeet Shandilya <avijit.shandilya@gmail.com>
2012-05-12 03:39:15 +0530

    data work

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,13 +12,18 @@ set :use_sudo, false
 set :application, "rubyonrails4ror"
 set :repository,  "git@github.com:awijeet/light.git"
 set :scm, :git
+set :scm_command, "/opt/local/bin/git" 
+set :local_scm_command, "git" 
 set :branch, 'master'
 set :deploy_to, "/var/apps/#{application}"
 
 role :web, "208.111.45.210"                          # Your HTTP server, Apache/etc
 role :app, "208.111.45.210"                          # This may be the same as your `Web` server
 role :db,  "208.111.45.210", primary: true # This is where Rails migrations will run
-
+before 'deploy:finalize_update', 'x:set_current_release'
+task :set_current_release, :roles => :app do
+    set :current_release, latest_release
+end
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
